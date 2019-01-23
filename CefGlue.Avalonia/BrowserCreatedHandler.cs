5efa71f547a8d59d2b0869b287a0582c32e69a18
zhongzf <zhongzf@126.com>
2019-01-23 22:04:29 +0800

    feat: add 3 event for application extending.

---
+++ CefGlue.Avalonia/BrowserCreatedHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xilium.CefGlue;
+
+namespace CefGlue.Avalonia
+{
+    public class BrowserCreatedEventArgs : EventArgs
+    {
+        public BrowserCreatedEventArgs(CefBrowser browser)
+        {
+            Browser = browser;
+        }
+
+        public CefBrowser Browser { get; private set; }
+    }
+
+    public delegate void BrowserCreatedHandler(object sender, BrowserCreatedEventArgs e);
+}
---
+++ CefGlue.Avalonia/RegisterCustomSchemesHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xilium.CefGlue;
+
+namespace CefGlue.Avalonia
+{
+    public class RegisterCustomSchemesEventArgs : EventArgs
+    {
+        public RegisterCustomSchemesEventArgs(CefSchemeRegistrar register)
+        {
+            Registrar = register;
+        }
+
+        public CefSchemeRegistrar Registrar { get; private set; }
+    }
+
+    public delegate void RegisterCustomSchemesHandler(object sender, RegisterCustomSchemesEventArgs e);
+}
